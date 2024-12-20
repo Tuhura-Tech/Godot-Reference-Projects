5500196cdc6a9049aaa5c1d80629e33bf8d727a1
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-20 16:35:03 +1300

    Update game script/scene
    Change preload to a constant
    Rename level_1 and for loop name
    Add comments
    Renamed node to be UpperCamelCase

--- 2D Platformer/Scripts/game.gd
+++ 2D Platformer/Scripts/game.gd
@@ -1,16 +1,19 @@
 extends Node2D
+## Level loader
 
 
-var level1 = preload("res://Scenes/level_1.tscn")
+# Load the first level as soon as the game is run to save time later
+const LEVEL_1 = preload("res://Scenes/level_1.tscn")
 
 
+# Load the first level
 func _ready() -> void:
-	var l = level1.instantiate()
-	l.connect("level1_finished", _on_level1_finished)
-	add_child(l)
+	var level_1 = LEVEL_1.instantiate()
+	level_1.connect("level1_finished", _on_level1_finished)
+	add_child(level_1)
 
 
+# Quit all levels when the first level is finished
 func _on_level1_finished():
-	for n in get_children():
-		remove_child(n)
-		n.queue_free()
+	for child in get_children():
+		child.queue_free()
