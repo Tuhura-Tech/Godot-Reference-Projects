456053dc2737c63e20a9ee7e6eb64e26528ebd4b
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-20 16:37:34 +1300

    Update coin scene/script
    Changed names to use UpperCamelCase and be more succinct
    Add comments
    Added _ to unused parameter

--- 2D Platformer/Scripts/coin.gd
+++ 2D Platformer/Scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
+## Coin that can be picked up, only prints +1 coin
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin") # Replace with function body.
+## Print +1 coin and free self
+func _on_body_entered(_body: Node2D) -> void:
+	print("+1 coin")
 	queue_free()
